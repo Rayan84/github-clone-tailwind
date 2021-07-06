a2b911a14445c5bc82027f27a57db3530e6f307e
Rayan84 <rayan_84@live.com>
2021-07-06 14:46:15 +0200

    Add icons to navbar

--- pages/github.vue
+++ pages/github.vue
@@ -19,16 +19,29 @@
           <li><a href="#" class="hover:text-gray-400">Explore</a></li>
         </ul>
       </div>
+      <div class="flex items-center space-x-4 text-white">
+        <a href="#" class="relative hover:text-gray-400">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+          </svg>
+          <div class="bg-blue-600 rounded-full w-2 h-2 absolute top-0 right-0"></div>
+        </a>
+        <a href="#" class="flex items-center hover:text-gray-400">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+          </svg>
+          <div class="bg-blue-600 rounded-full w-2 h-2 absolute top-0 right-0"></div>
+        </a>
+        <a href="#" class="flex items-center hover:text-gray-400">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+          </svg>
+          <div class="bg-blue-600 rounded-full w-2 h-2 absolute top-0 right-0"></div>
+        </a>
 
 
 
-
-
-
-
-
-
-      <div>right</div>
+      </div>
     </nav> 
   </div>  
 </template>
