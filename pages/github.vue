810f490471775c5b952a1335098ddb9e0f67372a
Rayan84 <rayan_84@live.com>
2021-07-06 07:40:18 +0200

    Add content to github.vue

---
+++ pages/github.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-grey-900 text-sm">
+    <nav class="bg-gray-900 text-white px-4 py-3 flex items-center justify-between"></nav>
+    github goes here    
+  </div>  
+</template>
