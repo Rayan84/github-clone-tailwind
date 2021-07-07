718b0d31176b99c16f377cd6baa3136ed641ea5e
Rayan84 <rayan_84@live.com>
2021-07-07 21:00:40 +0200

    Correct readme section position

--- pages/github.vue
+++ pages/github.vue
@@ -217,22 +217,136 @@
               <svg aria-label="Directory" aria-hidden="true" viewBox="0 0 16 16" data-view-component="true" height="16" width="16" class="fill-current text-blue-300 mr-3">
                 <path fill-rule="evenodd" d="M1.75 1A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25v-8.5A1.75 1.75 0 0014.25 3h-6.5a.25.25 0 01-.2-.1l-.9-1.2c-.33-.44-.85-.7-1.4-.7h-3.5z"></path>
               </svg>
-              <a href="#">.github</a>
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">.github</a>
             </div>
-            <div class="w-6/12">initial commit</div>
+            <div class="w-6/12">
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">initial commit</a>
+              </div>
             <div class="w-2/12 text-right">1 hour ago</div>           
           </div>
+
           <div class="flex justify-between px-4 py-2">
-            <div class="w-4/12">one</div>
-            
+            <div class="w-4/12 flex items-center">
+              <svg aria-label="Directory" aria-hidden="true" viewBox="0 0 16 16" data-view-component="true" height="16" width="16" class="fill-current text-blue-300 mr-3">
+                <path fill-rule="evenodd" d="M1.75 1A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25v-8.5A1.75 1.75 0 0014.25 3h-6.5a.25.25 0 01-.2-.1l-.9-1.2c-.33-.44-.85-.7-1.4-.7h-3.5z"></path>
+              </svg>
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">components</a>
+            </div>
+            <div class="w-6/12">
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">create nuxt app basic environment</a>
+              </div>
+            <div class="w-2/12 text-right">2 days ago</div>           
           </div>
+
           <div class="flex justify-between px-4 py-2">
-            <div class="w-4/12">one</div>
-            <div class="w-6/12">two sfsa</div>
-            <div class="w-2/12 text-right">three</div>
+            <div class="w-4/12 flex items-center">
+              <svg aria-label="Directory" aria-hidden="true" viewBox="0 0 16 16" data-view-component="true" height="16" width="16" class="fill-current text-blue-300 mr-3">
+                <path fill-rule="evenodd" d="M1.75 1A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25v-8.5A1.75 1.75 0 0014.25 3h-6.5a.25.25 0 01-.2-.1l-.9-1.2c-.33-.44-.85-.7-1.4-.7h-3.5z"></path>
+              </svg>
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">pages</a>
+            </div>
+            <div class="w-6/12">
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">add content and style to file explorer section</a>
+              </div>
+            <div class="w-2/12 text-right">29 minutes ago</div>           
+          </div>
+
+          <div class="flex justify-between px-4 py-2">
+            <div class="w-4/12 flex items-center">
+              <svg aria-label="Directory" aria-hidden="true" viewBox="0 0 16 16" data-view-component="true" height="16" width="16" class="fill-current text-blue-300 mr-3">
+                <path fill-rule="evenodd" d="M1.75 1A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25v-8.5A1.75 1.75 0 0014.25 3h-6.5a.25.25 0 01-.2-.1l-.9-1.2c-.33-.44-.85-.7-1.4-.7h-3.5z"></path>
+              </svg>
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">static</a>
+            </div>
+            <div class="w-6/12">
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">create nuxt app basic environment</a>
+              </div>
+            <div class="w-2/12 text-right">2 days ago</div>           
+          </div>
+
+          <div class="flex justify-between px-4 py-2">
+            <div class="w-4/12 flex items-center">
+              <svg aria-label="File" aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-file color-icon-tertiary">
+                <path fill-rule="evenodd" d="M3.75 1.5a.25.25 0 00-.25.25v11.5c0 .138.112.25.25.25h8.5a.25.25 0 00.25-.25V6H9.75A1.75 1.75 0 018 4.25V1.5H3.75zm5.75.56v2.19c0 .138.112.25.25.25h2.19L9.5 2.06zM2 1.75C2 .784 2.784 0 3.75 0h5.086c.464 0 .909.184 1.237.513l3.414 3.414c.329.328.513.773.513 1.237v8.086A1.75 1.75 0 0112.25 15h-8.5A1.75 1.75 0 012 13.25V1.75z"></path>
+              </svg>
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">.editorconfig</a>
+            </div>
+            <div class="w-6/12">
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">create nuxt app basic environment</a>
+              </div>
+            <div class="w-2/12 text-right">2 days ago</div>           
+          </div>
+
+          <div class="flex justify-between px-4 py-2">
+            <div class="w-4/12 flex items-center">
+              <svg aria-label="File" aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-file color-icon-tertiary">
+                <path fill-rule="evenodd" d="M3.75 1.5a.25.25 0 00-.25.25v11.5c0 .138.112.25.25.25h8.5a.25.25 0 00.25-.25V6H9.75A1.75 1.75 0 018 4.25V1.5H3.75zm5.75.56v2.19c0 .138.112.25.25.25h2.19L9.5 2.06zM2 1.75C2 .784 2.784 0 3.75 0h5.086c.464 0 .909.184 1.237.513l3.414 3.414c.329.328.513.773.513 1.237v8.086A1.75 1.75 0 0112.25 15h-8.5A1.75 1.75 0 012 13.25V1.75z"></path>
+              </svg>
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">.gitignore</a>
+            </div>
+            <div class="w-6/12">
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">create nuxt app basic environment</a>
+              </div>
+            <div class="w-2/12 text-right">2 days ago</div>           
+          </div>
+
+          <div class="flex justify-between px-4 py-2">
+            <div class="w-4/12 flex items-center">
+              <svg aria-label="File" aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-file color-icon-tertiary">
+                <path fill-rule="evenodd" d="M3.75 1.5a.25.25 0 00-.25.25v11.5c0 .138.112.25.25.25h8.5a.25.25 0 00.25-.25V6H9.75A1.75 1.75 0 018 4.25V1.5H3.75zm5.75.56v2.19c0 .138.112.25.25.25h2.19L9.5 2.06zM2 1.75C2 .784 2.784 0 3.75 0h5.086c.464 0 .909.184 1.237.513l3.414 3.414c.329.328.513.773.513 1.237v8.086A1.75 1.75 0 0112.25 15h-8.5A1.75 1.75 0 012 13.25V1.75z"></path>
+              </svg>
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">README.md</a>
+            </div>
+            <div class="w-6/12">
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">Add commits container</a>
+              </div>
+            <div class="w-2/12 text-right">13 hour ago</div>           
+          </div>
+
+          <div class="flex justify-between px-4 py-2">
+            <div class="w-4/12 flex items-center">
+              <svg aria-label="File" aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-file color-icon-tertiary">
+                <path fill-rule="evenodd" d="M3.75 1.5a.25.25 0 00-.25.25v11.5c0 .138.112.25.25.25h8.5a.25.25 0 00.25-.25V6H9.75A1.75 1.75 0 018 4.25V1.5H3.75zm5.75.56v2.19c0 .138.112.25.25.25h2.19L9.5 2.06zM2 1.75C2 .784 2.784 0 3.75 0h5.086c.464 0 .909.184 1.237.513l3.414 3.414c.329.328.513.773.513 1.237v8.086A1.75 1.75 0 0112.25 15h-8.5A1.75 1.75 0 012 13.25V1.75z"></path>
+              </svg>
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">nuxt.config.js</a>
+            </div>
+            <div class="w-6/12">
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">create nuxt app basic environment</a>
+              </div>
+            <div class="w-2/12 text-right">2 days ago</div>           
+          </div>
+
+          <div class="flex justify-between px-4 py-2">
+            <div class="w-4/12 flex items-center">
+              <svg aria-label="File" aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-file color-icon-tertiary">
+                <path fill-rule="evenodd" d="M3.75 1.5a.25.25 0 00-.25.25v11.5c0 .138.112.25.25.25h8.5a.25.25 0 00.25-.25V6H9.75A1.75 1.75 0 018 4.25V1.5H3.75zm5.75.56v2.19c0 .138.112.25.25.25h2.19L9.5 2.06zM2 1.75C2 .784 2.784 0 3.75 0h5.086c.464 0 .909.184 1.237.513l3.414 3.414c.329.328.513.773.513 1.237v8.086A1.75 1.75 0 0112.25 15h-8.5A1.75 1.75 0 012 13.25V1.75z"></path>
+              </svg>
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">package-lock.json</a>
+            </div>
+            <div class="w-6/12">
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">create nuxt app basic environment</a>
+              </div>
+            <div class="w-2/12 text-right">2 days ago</div>           
+          </div>
+
+          <div class="flex justify-between px-4 py-2">
+            <div class="w-4/12 flex items-center">
+              <svg aria-label="File" aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-file color-icon-tertiary">
+                <path fill-rule="evenodd" d="M3.75 1.5a.25.25 0 00-.25.25v11.5c0 .138.112.25.25.25h8.5a.25.25 0 00.25-.25V6H9.75A1.75 1.75 0 018 4.25V1.5H3.75zm5.75.56v2.19c0 .138.112.25.25.25h2.19L9.5 2.06zM2 1.75C2 .784 2.784 0 3.75 0h5.086c.464 0 .909.184 1.237.513l3.414 3.414c.329.328.513.773.513 1.237v8.086A1.75 1.75 0 0112.25 15h-8.5A1.75 1.75 0 012 13.25V1.75z"></path>
+              </svg>
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">package.json</a>
+            </div>
+            <div class="w-6/12">
+              <a href="#" class="hover:underline hover:text-blue-500 ml-3">create nuxt app basic environment</a>
+              </div>
+            <div class="w-2/12 text-right">2 days ago</div>           
           </div>
         </div>
-        
+      <div class="readme-container rounded-md border border-gray-300 px-4 py-4 mt-4">
+        <h4 class="font-semibold">README.md</h4>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor iure quo tempore? Odio minima voluptate eveniet, omnis quisquam voluptatibus, numquam aliquam sint et quo excepturi, nulla enim incidunt dolores asperiores!
+        </div>
+      </div> <!-- end of readme section -->
       </div> <!-- end of file explorer container -->
       <div class="sidebar w-1/4">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus inventore dolor qui eveniet quam quidem, architecto illum aperiam et magni impedit alias minus expedita, laboriosam laudantium fuga, molestiae consequatur repellendus. Cum culpa nesciunt commodi rerum nam repellat, soluta consequuntur et corporis ipsam ab natus, quibusdam minus, iste illum ducimus! Laborum consectetur repellat sequi voluptatem ipsam possimus nam quo, placeat rerum in? A, impedit blanditiis eius architecto ex in neque laudantium sapiente exercitationem. Cumque necessitatibus culpa laudantium provident consequuntur fuga recusandae, sint tenetur soluta iure voluptatum? Voluptate, veritatis quisquam! Officia rerum dignissimos consequuntur necessitatibus doloribus laudantium voluptatum harum doloremque ratione placeat.
