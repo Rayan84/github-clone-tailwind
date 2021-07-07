63ec127f5ba0fb68b95bdfabad19fe3aeac57a07
Rayan84 <rayan_84@live.com>
2021-07-08 00:21:25 +0200

    Add content and style to the sidebar

--- pages/github.vue
+++ pages/github.vue
@@ -42,7 +42,7 @@
         </a>
       </div>
     </nav> 
-
+    <div class="bg-gray-50 ">
     <div class="repo-stats flex items-center justify-between px-8 py-4">
       <div class="flex">
         <svg class="w-4 fill-current" viewBox="0 0 16 16" aria-hidden="true"><path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"></path></svg>
@@ -84,7 +84,7 @@
         </div>
       </div>
     </div> <!-- end of stats bar -->
-    <ul class="repo-nav border-b border-gray-400 flex items-center px-8 mt-4">
+    <ul class="repo-nav border-b border-gray-200 flex items-center px-8 mt-4">
       <li class="font-semibold">
         <a href="#" class="flex items-center border-b-2 border-red-500 px-4 pb-3">
           <svg class="octicon octicon-code UnderlineNav-octicon d-none d-sm-inline" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M4.72 3.22a.75.75 0 011.06 1.06L2.06 8l3.72 3.72a.75.75 0 11-1.06 1.06L.47 8.53a.75.75 0 010-1.06l4.25-4.25zm6.56 0a.75.75 0 10-1.06 1.06L13.94 8l-3.72 3.72a.75.75 0 101.06 1.06l4.25-4.25a.75.75 0 000-1.06l-4.25-4.25z"></path></svg>
@@ -142,7 +142,8 @@
         </li>
       </li>
     </ul> <!-- end of repo navbar -->
-    <div class="container mx-auto my-8 px-4 flex">
+    </div>
+    <div class="container mx-auto	my-8 px-4 flex m-auto max-w-screen-xl">
       <div class="file-explorer-container w-3/4 mr-8">
         <div class="branch-navigation flex items-center justify-between">
           <div class="flex items-center space-x-3">
@@ -185,7 +186,7 @@
             </button>
           </div>
         </div>
-        <div class="commits-container bg-blue-100 rounded-md rounded-b-none border border-blue-200 border-b-0 flex items-center justify-between px-4 py-4 mt-5">
+        <div class="commits-container bg-blue-50 rounded-md rounded-b-none border border-blue-200 border-b-0 flex items-center justify-between px-4 py-4 mt-5">
           <div class="flex items-center space-x-2">
             <a href="#">
               <img src="https://avatars.githubusercontent.com/u/5465074?v=4" alt="avatar" class="rounded-full w-6 h-6">
@@ -349,12 +350,77 @@
       </div> <!-- end of readme section -->
       </div> <!-- end of file explorer container -->
       <div class="sidebar w-1/4">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus inventore dolor qui eveniet quam quidem, architecto illum aperiam et magni impedit alias minus expedita, laboriosam laudantium fuga, molestiae consequatur repellendus. Cum culpa nesciunt commodi rerum nam repellat, soluta consequuntur et corporis ipsam ab natus, quibusdam minus, iste illum ducimus! Laborum consectetur repellat sequi voluptatem ipsam possimus nam quo, placeat rerum in? A, impedit blanditiis eius architecto ex in neque laudantium sapiente exercitationem. Cumque necessitatibus culpa laudantium provident consequuntur fuga recusandae, sint tenetur soluta iure voluptatum? Voluptate, veritatis quisquam! Officia rerum dignissimos consequuntur necessitatibus doloribus laudantium voluptatum harum doloremque ratione placeat.
+        <div class="pb-5  border-b-2">
+          <div class="flex justify-between">
+            <h3 class="text-lg mb-3">About</h3>
+            <svg aria-label="Edit repository metadata" role="img" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-gear float-right">
+              <path fill-rule="evenodd" d="M7.429 1.525a6.593 6.593 0 011.142 0c.036.003.108.036.137.146l.289 1.105c.147.56.55.967.997 1.189.174.086.341.183.501.29.417.278.97.423 1.53.27l1.102-.303c.11-.03.175.016.195.046.219.31.41.641.573.989.014.031.022.11-.059.19l-.815.806c-.411.406-.562.957-.53 1.456a4.588 4.588 0 010 .582c-.032.499.119 1.05.53 1.456l.815.806c.08.08.073.159.059.19a6.494 6.494 0 01-.573.99c-.02.029-.086.074-.195.045l-1.103-.303c-.559-.153-1.112-.008-1.529.27-.16.107-.327.204-.5.29-.449.222-.851.628-.998 1.189l-.289 1.105c-.029.11-.101.143-.137.146a6.613 6.613 0 01-1.142 0c-.036-.003-.108-.037-.137-.146l-.289-1.105c-.147-.56-.55-.967-.997-1.189a4.502 4.502 0 01-.501-.29c-.417-.278-.97-.423-1.53-.27l-1.102.303c-.11.03-.175-.016-.195-.046a6.492 6.492 0 01-.573-.989c-.014-.031-.022-.11.059-.19l.815-.806c.411-.406.562-.957.53-1.456a4.587 4.587 0 010-.582c.032-.499-.119-1.05-.53-1.456l-.815-.806c-.08-.08-.073-.159-.059-.19a6.44 6.44 0 01.573-.99c.02-.029.086-.075.195-.045l1.103.303c.559.153 1.112.008 1.529-.27.16-.107.327-.204.5-.29.449-.222.851-.628.998-1.189l.289-1.105c.029-.11.101-.143.137-.146zM8 0c-.236 0-.47.01-.701.03-.743.065-1.29.615-1.458 1.261l-.29 1.106c-.017.066-.078.158-.211.224a5.994 5.994 0 00-.668.386c-.123.082-.233.09-.3.071L3.27 2.776c-.644-.177-1.392.02-1.82.63a7.977 7.977 0 00-.704 1.217c-.315.675-.111 1.422.363 1.891l.815.806c.05.048.098.147.088.294a6.084 6.084 0 000 .772c.01.147-.038.246-.088.294l-.815.806c-.474.469-.678 1.216-.363 1.891.2.428.436.835.704 1.218.428.609 1.176.806 1.82.63l1.103-.303c.066-.019.176-.011.299.071.213.143.436.272.668.386.133.066.194.158.212.224l.289 1.106c.169.646.715 1.196 1.458 1.26a8.094 8.094 0 001.402 0c.743-.064 1.29-.614 1.458-1.26l.29-1.106c.017-.066.078-.158.211-.224a5.98 5.98 0 00.668-.386c.123-.082.233-.09.3-.071l1.102.302c.644.177 1.392-.02 1.82-.63.268-.382.505-.789.704-1.217.315-.675.111-1.422-.364-1.891l-.814-.806c-.05-.048-.098-.147-.088-.294a6.1 6.1 0 000-.772c-.01-.147.039-.246.088-.294l.814-.806c.475-.469.679-1.216.364-1.891a7.992 7.992 0 00-.704-1.218c-.428-.609-1.176-.806-1.82-.63l-1.103.303c-.066.019-.176.011-.299-.071a5.991 5.991 0 00-.668-.386c-.133-.066-.194-.158-.212-.224L10.16 1.29C9.99.645 9.444.095 8.701.031A8.094 8.094 0 008 0zm1.5 8a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM11 8a3 3 0 11-6 0 3 3 0 016 0z"></path>
+            </svg>
+          </div>
+          <div>
+            <p>No description, website, or topics provided.</p>
+          </div>
+          <div class=""> 
+            <a class="flex items-end" href="#readme">
+            <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-book mr-2">
+                <path fill-rule="evenodd" d="M0 1.75A.75.75 0 01.75 1h4.253c1.227 0 2.317.59 3 1.501A3.744 3.744 0 0111.006 1h4.245a.75.75 0 01.75.75v10.5a.75.75 0 01-.75.75h-4.507a2.25 2.25 0 00-1.591.659l-.622.621a.75.75 0 01-1.06 0l-.622-.621A2.25 2.25 0 005.258 13H.75a.75.75 0 01-.75-.75V1.75zm8.755 3a2.25 2.25 0 012.25-2.25H14.5v9h-3.757c-.71 0-1.4.201-1.992.572l.004-7.322zm-1.504 7.324l.004-5.073-.002-2.253A2.25 2.25 0 005.003 2.5H1.5v9h3.757a3.75 3.75 0 011.994.574z"></path>
+              </svg>
+                Readme
+            </a> 
+          </div>
+        </div>
+
+        <div class="py-5  border-b-2">
+          <h3 class="text-lg mb-3">Releases</h3>
+           <p>No releases published</p>
+           <a href="#" class="text-blue-500 hover:underline hover:text-blue-700"> Create a new release</a>
+        </div>
+
+        <div class="py-5  border-b-2">
+          <h3 class="text-lg mb-3">Packages</h3>
+           <p>No packages published</p>
+           <a href="#" class="text-blue-500 hover:underline hover:text-blue-700"> Publish your first package</a>
+        </div>
+
+        <div class="py-7  border-b-2">
+          <h3 class="text-lg mb-3">Environment
+            <span class="text-xs rounded-lg bg-gray-200 px-2 ml-1">2
+            </span>
+          </h3>
+          <div class="flex items-end">
+            <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-rocket">
+              <path fill-rule="evenodd" d="M14.064 0a8.75 8.75 0 00-6.187 2.563l-.459.458c-.314.314-.616.641-.904.979H3.31a1.75 1.75 0 00-1.49.833L.11 7.607a.75.75 0 00.418 1.11l3.102.954c.037.051.079.1.124.145l2.429 2.428c.046.046.094.088.145.125l.954 3.102a.75.75 0 001.11.418l2.774-1.707a1.75 1.75 0 00.833-1.49V9.485c.338-.288.665-.59.979-.904l.458-.459A8.75 8.75 0 0016 1.936V1.75A1.75 1.75 0 0014.25 0h-.186zM10.5 10.625c-.088.06-.177.118-.266.175l-2.35 1.521.548 1.783 1.949-1.2a.25.25 0 00.119-.213v-2.066zM3.678 8.116L5.2 5.766c.058-.09.117-.178.176-.266H3.309a.25.25 0 00-.213.119l-1.2 1.95 1.782.547zm5.26-4.493A7.25 7.25 0 0114.063 1.5h.186a.25.25 0 01.25.25v.186a7.25 7.25 0 01-2.123 5.127l-.459.458a15.21 15.21 0 01-2.499 2.02l-2.317 1.5-2.143-2.143 1.5-2.317a15.25 15.25 0 012.02-2.5l.458-.458h.002zM12 5a1 1 0 11-2 0 1 1 0 012 0zm-8.44 9.56a1.5 1.5 0 10-2.12-2.12c-.734.73-1.047 2.332-1.15 3.003a.23.23 0 00.265.265c.671-.103 2.273-.416 3.005-1.148z"></path>
+            </svg>
+            <a href="#" class="hover:underline hover:text-blue-500 ml-1 font-bold text-gray-600">github-pages</a>
+            <span class="border rounded text-green-500 ml-2">Active</span>
+          </div>
+        </div>
       </div> <!-- end of sidebar -->
     </div>
-    <footer class="container mx-auto text-xs border-t border-gray-400 py-10 flex items-center justify-between">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, esse in quo nam commodi corrupti voluptatibus, iure et, reprehenderit accusamus eveniet. Provident molestias animi earum, error autem minus magni voluptatibus totam exercitationem. Incidunt doloribus ut quos cumque commodi tempore natus. Deleniti odit officiis et vitae aperiam ad iure commodi vel autem libero. Amet maiores quia ex non consectetur. Rerum sit consequatur in nesciunt! Cumque veritatis officiis, eveniet quam quae voluptatibus dolores ipsam natus voluptas, laboriosam eos delectus blanditiis repellat temporibus est vel saepe! Praesentium mollitia dolores consequuntur adipisci maiores dolore placeat eius est dicta distinctio, in illo qui debitis officiis, natus similique. Esse tenetur asperiores assumenda ducimus ad laborum incidunt fuga harum doloribus neque nam odio a accusamus modi, vero omnis qui beatae quibusdam veniam accusantium est eligendi laudantium. Quam temporibus saepe, totam asperiores dolores placeat corporis, aliquam iste voluptas fugiat ratione, quas repellendus illo? Nihil ea dolore ipsa architecto ipsam aut quasi. Nesciunt vel obcaecati exercitationem corporis deleniti, illo officia, voluptate optio voluptas repellendus atque laudantium dolore soluta. Neque corporis quasi architecto quis. Omnis quam obcaecati perspiciatis eos et nisi quod blanditiis esse, asperiores sunt totam minus dolores facere illo nobis neque aliquid iusto ducimus explicabo. Ipsa, consequuntur expedita?
-
+    <footer class="container mx-auto text-xs border-t  py-10 flex items-center justify-between max-w-screen-xl">
+      <ul class="flex items-center space-x-12">
+        <li>&copy; 2020 GitHub, Inc.</li>
+        <li><a href="#" class="text-blue-600 hover:underline">Terms</a></li>
+        <li><a href="#" class="text-blue-600 hover:underline">Privacy</a></li>
+        <li><a href="#" class="text-blue-600 hover:underline">Security</a></li>
+        <li><a href="#" class="text-blue-600 hover:underline">Status</a></li>
+        <li><a href="#" class="text-blue-600 hover:underline">Docs</a></li>
+      </ul>
+      <div>
+        <a href="#">
+          <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="24" width="24" class="octicon octicon-mark-github">
+            <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+          </svg>
+        </a>
+      </div>
+      <ul class="flex items-center space-x-12">
+        <li><a href="#" class="text-blue-600 hover:underline">Contact GitHub</a></li>
+        <li><a href="#" class="text-blue-600 hover:underline">Pricing</a></li>
+        <li><a href="#" class="text-blue-600 hover:underline">API</a></li>
+        <li><a href="#" class="text-blue-600 hover:underline">Training</a></li>
+        <li><a href="#" class="text-blue-600 hover:underline">Blog</a></li>
+        <li><a href="#" class="text-blue-600 hover:underline">About</a></li>
+      </ul>
     </footer>
   </div>  
 </template>
