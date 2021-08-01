c79dd9ab899543a230753384c29b54b057906616
Rayan84 <rayan_84@live.com>
2021-08-01 20:39:23 +0200

    Add content and style to the sidebar

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -322,16 +322,33 @@
           </div>
           <div class="tags text-xs font-semibold flex items-center flex-wrap mt-2">
             <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">tailwindcss</a>
-            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">tailwindcss</a>
-            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">tailwindcss</a>
-            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">tailwindcss</a>
-            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">tailwindcss</a>
-            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">tailwindcss</a>
-            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">tailwindcss</a>
-
+            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">css</a>
+            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">css-framework</a>
+            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">postcss</a>
+            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">functional-css</a>
+            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">utility-classes</a>
+            <a href="#" class="bg-blue-100 hover:200 text-blue-600 rounded-full px-2 py-1 mt-2">responsive</a>
           </div>
-
+          <a href="#" class="flex items-center space-x-2 mt-4 group">
+            <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-book mr-2">
+              <path fill-rule="evenodd" d="M0 1.75A.75.75 0 01.75 1h4.253c1.227 0 2.317.59 3 1.501A3.744 3.744 0 0111.006 1h4.245a.75.75 0 01.75.75v10.5a.75.75 0 01-.75.75h-4.507a2.25 2.25 0 00-1.591.659l-.622.621a.75.75 0 01-1.06 0l-.622-.621A2.25 2.25 0 005.258 13H.75a.75.75 0 01-.75-.75V1.75zm8.755 3a2.25 2.25 0 012.25-2.25H14.5v9h-3.757c-.71 0-1.4.201-1.992.572l.004-7.322zm-1.504 7.324l.004-5.073-.002-2.253A2.25 2.25 0 005.003 2.5H1.5v9h3.757a3.75 3.75 0 011.994.574z"></path>
+            </svg>
+            <span class="text-grey-600 group-hover:text-blue-600">Readme</span>
+          </a>
+          <a href="#" class="flex items-center space-x-2 mt-4 group">
+            <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-law mr-2">
+              <path fill-rule="evenodd" d="M8.75.75a.75.75 0 00-1.5 0V2h-.984c-.305 0-.604.08-.869.23l-1.288.737A.25.25 0 013.984 3H1.75a.75.75 0 000 1.5h.428L.066 9.192a.75.75 0 00.154.838l.53-.53-.53.53v.001l.002.002.002.002.006.006.016.015.045.04a3.514 3.514 0 00.686.45A4.492 4.492 0 003 11c.88 0 1.556-.22 2.023-.454a3.515 3.515 0 00.686-.45l.045-.04.016-.015.006-.006.002-.002.001-.002L5.25 9.5l.53.53a.75.75 0 00.154-.838L3.822 4.5h.162c.305 0 .604-.08.869-.23l1.289-.737a.25.25 0 01.124-.033h.984V13h-2.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-2.5V3.5h.984a.25.25 0 01.124.033l1.29.736c.264.152.563.231.868.231h.162l-2.112 4.692a.75.75 0 00.154.838l.53-.53-.53.53v.001l.002.002.002.002.006.006.016.015.045.04a3.517 3.517 0 00.686.45A4.492 4.492 0 0013 11c.88 0 1.556-.22 2.023-.454a3.512 3.512 0 00.686-.45l.045-.04.01-.01.006-.005.006-.006.002-.002.001-.002-.529-.531.53.53a.75.75 0 00.154-.838L13.823 4.5h.427a.75.75 0 000-1.5h-2.234a.25.25 0 01-.124-.033l-1.29-.736A1.75 1.75 0 009.735 2H8.75V.75zM1.695 9.227c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L3 6.327l-1.305 2.9zm10 0c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L13 6.327l-1.305 2.9z"></path>
+            </svg>
+            <span class="text-grey-600 group-hover:text-blue-600">MIT License</span>
+          </a>
+        </div> <!-- end about -->
+        <div class="releases border-t border-gray-300 mt-6 pt-6">
+          <h4 class="text-base font-semibold flex items-center">
+            <span>Releases</span>
+            <div class="text-xs rounded-lg bg-gray-300 px-2 ml-1">79</div>
+          </h4>
         </div>
+
       
       </div>
 
