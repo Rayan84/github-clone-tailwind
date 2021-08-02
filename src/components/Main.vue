409800f2fca982838e2bc2336dd5a868f2248954
Rayan84 <rayan_84@live.com>
2021-08-02 23:06:14 +0200

    Finish the sidebar

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -406,6 +406,19 @@
             <div class="w-11/12 h-2 rounded-md rounded-r-none bg-purple-400"></div>
             <div class="w-1/12 h-2 rounded-md rounded-l-none bg-yellow-400"></div>
           </div>
+          <ul class="mt-2 flex space-x-8 items-center text-xs">
+            <li class="flex items-center">
+              <span class="w-2 h-2 rounded-full bg-purple-700"></span>
+              <span class="mt-1 ml-3">
+                <span class="font-semibold">CSS</span>
+                <span>93.9%</span>
+              </span>
+               <span class="mt-1 ml-16">
+                <span class="font-semibold">JavaScript</span>
+                <span>6.1%</span>
+              </span>
+            </li>
+          </ul>
         </div>  <!-- end of the languages -->
       </div>    <!-- end of the sidebar -->
 
